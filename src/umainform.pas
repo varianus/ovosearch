eb7d7d58c4cfcaf1f19b1430133514746f7e9ae5
varianus <varianus@users.noreply.github.com>
2019-04-22 20:12:31 +0200

    Initial release

---
+++ src/umainform.pas
@@ -0,0 +1,38 @@
+unit uMainForm;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls, ComCtrls,
+  StdCtrls, AnchorDockPanel;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Button1: TButton;
+    lbContaining: TLabeledEdit;
+    lbFiles: TLabeledEdit;
+    lbPath: TLabeledEdit;
+    ListView1: TListView;
+    Panel1: TPanel;
+    Splitter1: TSplitter;
+    Splitter2: TSplitter;
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
